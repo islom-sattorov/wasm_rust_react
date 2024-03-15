058fe8b42e6256e08f0e95b9e71e75aa9c3274d9
sia <sia@MacBook-Air---Islom-2.local>
2024-03-15 06:30:15 +0500

    init commit

--- wasm_react/src/App.tsx
+++ wasm_react/src/App.tsx
@@ -1,11 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { find_max } from "../public/pkg/wasm_test";
+// import init from "../public/pkg/wasm_test_bg.wasm";
+import "./App.css";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const arr = [
+    1, 123, 153, 345, 45, 654, 4, 325, 546, 5, 56, 67, 8, 3, 453, 5, 34, 32,
+  ];
 
+  // console.log(init);
+
+  console.log(find_max(arr));
   return (
     <>
       <div>
@@ -29,7 +37,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
